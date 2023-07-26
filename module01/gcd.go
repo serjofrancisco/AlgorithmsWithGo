f4cf396ad7c6305db8198bf4cb935d0071488543
Sergio Francisco <spinheiro.lf@gmail.com>
2023-07-26 20:29:33 -0300

    Adiciona Greatest Common Divisor

--- module01/gcd.go
+++ module01/gcd.go
@@ -1,5 +1,17 @@
 package module01
 
+// func GCD(a, b int) int {
+// 	if b == 0 {
+// 		return a;
+// 	}
+// 	return GCD(b, a % b);
+// }
+
+//iterative solution
+
 func GCD(a, b int) int {
-	return 0
+	for b != 0 {
+		a, b = b, a % b;
+	}
+	return a;
 }
