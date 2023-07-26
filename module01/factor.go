4d5fc54e0ff5f9d733f04271175e8a95f453e4ed
Sergio Francisco <spinheiro.lf@gmail.com>
2023-07-26 20:20:21 -0300

    Adiciona fib

--- module01/factor.go
+++ module01/factor.go
@@ -11,28 +11,27 @@ package module01
 //
 // Examples:
 //
-//   Factor([]int{2,3,5}, 30) // []int{2,3,5}
-//   Factor([]int{2,3,5}, 28) // []int{2,2,7}
-//   Factor([]int{2,3,5}, 720) // []int{2,2,2,2,3,3,5}
+//	Factor([]int{2,3,5}, 30) // []int{2,3,5}
+//	Factor([]int{2,3,5}, 28) // []int{2,2,7}
+//	Factor([]int{2,3,5}, 720) // []int{2,2,2,2,3,3,5}
 //
 // Examples with remainders:
 //
-//   Factor([2,5], 30) // []int{2,5,3}
-//   Factor([3,5], 720) // []int{3,3,5,16}
-//   Factor([], 4) // []int{4}
-//
+//	Factor([2,5], 30) // []int{2,5,3}
+//	Factor([3,5], 720) // []int{3,3,5,16}
+//	Factor([], 4) // []int{4}
 func Factor(primes []int, number int) []int {
-	var factors []int;
+	var factors []int
 	for _, v := range primes {
-		for number % v == 0 {
-			factors = append(factors, v);
-			number /= v;
+		for number%v == 0 && v != 1 {
+			factors = append(factors, v)
+			number /= v
 		}
 	}
 	if number > 1 {
-		factors = append(factors, number);
+		factors = append(factors, number)
 	}
-	return factors;
+	return factors
 }
 
 //for in go can function like while in other languages
--- module01/fibonacci.go
+++ module01/fibonacci.go
@@ -23,7 +23,31 @@ package module01
 //   Fibonacci(6) => 8
 //   Fibonacci(7) => 13
 //   Fibonacci(14) => 377
-//
+//Recursive	solution
+// func Fibonacci(n int) int {
+// 	if n == 0 {
+// 		return 0;
+// 	}
+// 	if n == 1 {
+// 		return 1;
+// 	}
+// 	return Fibonacci(n - 1) + Fibonacci(n - 2);
+// }
+//Iterative solution more confusing but faster
 func Fibonacci(n int) int {
-	return 0
+	if n == 0 {
+		return 0;
+	}
+	if n == 1 {
+		return 1;
+	}
+	var prev1 int = 1;
+	var prev2 int = 0;
+	var result int = 0;
+	for i := 2; i <= n; i++ {
+		result = prev1 + prev2;
+		prev2 = prev1;
+		prev1 = result;
+	}
+	return result;
 }
